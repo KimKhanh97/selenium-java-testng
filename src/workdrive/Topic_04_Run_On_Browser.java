4bd682ac9b6ee6c7c5f0a776151c354b55976611
This PC <This PC@192.168.1.21>
2023-06-28 19:19:34 +0700

    Run on browser

--- src/workdrive/Topic_04_Run_On_Browser.java
+++ src/workdrive/Topic_04_Run_On_Browser.java
@@ -1,61 +1,52 @@
 package workdrive;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Topic_04_Run_On_Browser {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-
-	@BeforeClass
-	public void beforeClass() {
-		//Executable file
+	
+	@Test
+	public void TC_01_Run_Chrome() {
 		//Chrome
-		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		if(osName.contains("Windows")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		} else {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		}
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
+		driver.get("https://demo.nopcommerce.com/");
+		driver.quit();
+	}
+
+	@Test
+	public void TC_02_Run_Firefox() {
 		//Firefox
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		if(osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
 		driver = new FirefoxDriver();
-		
+		driver.get("https://demo.nopcommerce.com/");
+		driver.quit();
+	}
+
+	@Test
+	public void TC_03_Run_Edge() {
 		//Edge
-		System.setProperty("webdriver.Edge.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-		driver = new FirefoxDriver();
-	}
-
-	@Test
-	public void TC_01_Empty_Data() {
-	}
-
-	@Test
-	public void TC_02_Invalid_Email() {
-	}
-
-	@Test
-	public void TC_03_Incorrect_Email() {
-	}
-	
-	@Test
-	public void TC_04_Invalid_Password() {
-	}
-	
-	@Test
-	public void TC_05_Incorrect_Password() {
-	}
-	
-	@Test
-	public void TC_06_Invalid_Phone() {
-	}
-
-	@AfterClass
-	public void afterClass() {
+		if(osName.contains("Windows")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		} else {
+			System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
+		}
+		driver = new EdgeDriver();
+		driver.get("https://demo.nopcommerce.com/");
 		driver.quit();
 	}
 }
