169a0952f322b8e4fa203ff8812a98b4844b5b0f
TAVS-71 <TAVS-71@LAPTOP-HTJM8IQG>
2023-07-03 17:57:39 +0700

    Topic 06

---
+++ src/workdrive/Topic_06_Web_Element.java
@@ -0,0 +1,51 @@
+package workdrive;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_06_Web_Element {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	WebElement emailTextbox = driver.findElement(By.id("Email"));
+	WebElement passwordTextbox = driver.findElement(By.id("Password"));
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01_WebElement() {
+		driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
+		
+		emailTextbox.sendKeys("");
+		passwordTextbox.sendKeys("");
+		
+		
+		// Tương tác được với Element thì cần phải tìm được element đó
+		// Thông qua các locator của nó
+		
+		//By: id/class/name/xpath/css/tagname/linkText/partialLinkText
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
