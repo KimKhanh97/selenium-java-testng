69bd5063961f55f2a2004437700a9457321cb761
This PC <This PC@192.168.1.21>
2023-07-27 21:41:45 +0700

    update

--- src/workdrive/Topic_08_Default_Dropdown.java
+++ src/workdrive/Topic_08_Default_Dropdown.java
@@ -1,10 +1,13 @@
 package workdrive;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,7 +16,7 @@ public class Topic_08_Default_Dropdown {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-	String firstName, lastName, emailAddress, companyName, password;
+	String firstName, lastName, emailAddress, companyName, password, day, month, year;
 
 	@BeforeClass
 	public void beforeClass() {
@@ -28,9 +31,12 @@ public class Topic_08_Default_Dropdown {
 		driver.manage().window().maximize();
 		firstName = "Khanh";
 		lastName = "Kim";
-		emailAddress = "trankimkhanh0909@gmail.com";
+		emailAddress = "trankimkhanh" + getRandomNumber() + "@gmail.com";
 		companyName = "TechAvenueSolution";
 		password = "Kimkhanh@090997";
+		day = "18";
+		month = "December";
+		year = "1994";
 	}
 
 	@Test
@@ -40,10 +46,47 @@ public class Topic_08_Default_Dropdown {
 		driver.findElement(By.id("FirstName")).sendKeys(firstName);
 		driver.findElement(By.id("LastName")).sendKeys(lastName);
 
+		// Dùng để chọn: selectByIndex; selectByValue; selectByVisibleText
+		// Dùng để bỏ chọn: deselectByIndex; deselectByValue; deselectByVisibleText
+		// Dùng trong trường hơp dropdown có thể chọn nhiều value (mutiple dropdown):
+		// deselectAll
+		// Get toàn bộ những option được chọn trong dropdown: getAllSelectedOptions
+		// Get option đầu tiên được chọn: getFirstSelectedOption
+		// Dùng cho trường hợp get list option trong dropdown: getOptions
+		// Kiểm tra xem dropdown là single/F hay multiple/T: isMultiple
+
+		new Select(driver.findElement(By.name("DateOfBirthDay"))).selectByVisibleText(day);
+		new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText(month);
+		new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText(year);
+
+		driver.findElement(By.id("Email")).sendKeys(emailAddress);
+		driver.findElement(By.id("Company")).sendKeys(companyName);
+		driver.findElement(By.id("Password")).sendKeys(password);
+		driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+		
+		driver.findElement(By.id("register-button")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='result']")).getText(), "Your registration completed");
+		driver.findElement(By.cssSelector("a.ico-account")).click();
+		
+		//Verify
+		Assert.assertEquals(driver.findElement(By.id("FirstName")).getAttribute("value"), firstName);
+		Assert.assertEquals(driver.findElement(By.id("LastName")).getAttribute("value"), lastName);
+		
+		
+		Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthDay"))).getFirstSelectedOption().getText(), day);
+		Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthMonth"))).getFirstSelectedOption().getText(), month);
+		Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthYear"))).getFirstSelectedOption().getText(), year);
+		Assert.assertEquals(driver.findElement(By.id("Company")).getAttribute("value"), companyName);
 	}
 
 	@Test
 	public void TC_02_Add_Address() {
+	}
+
+	public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(99999);
 	}
 
 	// 1000ms = 1s
@@ -57,6 +100,6 @@ public class Topic_08_Default_Dropdown {
 
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+		//driver.quit();
 	}
 }
