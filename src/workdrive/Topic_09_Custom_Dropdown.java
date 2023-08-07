ff31044c9dd32cc5cda0c94f552298300f0d77cf
This PC <This PC@192.168.1.21>
2023-08-07 21:27:17 +0700

    update

--- src/workdrive/Topic_09_Custom_Dropdown.java
+++ src/workdrive/Topic_09_Custom_Dropdown.java
@@ -36,47 +36,59 @@ public class Topic_09_Custom_Dropdown {
 	@Test
 	public void TC_01_Jquery_01() {
 		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
-        //1 - Click vào 1 thẻ bất kì để làm sao cho nó xổ ra hết các item của dropdown
-		driver.findElement(By.cssSelector("span#speed-button")).click();
-        //2 - Chò cho tất cả các item được load ra thành công
-		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("ul#speed-menu div[role='option']")));
 		
-		//Đưa hết tất cả item trong dropdown vào trong 1 list
-		List<WebElement> speedDropdownItems = driver.findElements(By.cssSelector("ul#speed-menu div[role='option']"));
-		
-		//3 - Tìm item xem đúng cái đang cần hay không(dùng vòng lặp duyệt qua)
-		for (WebElement tempItem : speedDropdownItems) {
-			String itemText = tempItem.getText();
-			System.out.println(itemText);
-		//4 - Kiểm tra cái text của item đúng với cái mình mong muốn
-			if (itemText.equals("Faster")) {
-		//5 -  Click vào item đó
-			System.out.println("Click vào item");
-				tempItem.click();
-				// Thoát ra khỏi vòng lặp không xét cho các case 
-				break;
-			} else {
-				System.out.println("Không click vào item");
-			}
-		}
-       
+		//Muốn chọn item cho Speed dropdown
+		selectItemInDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Slower");
+		sleepInSecond(3);
+		selectItemInDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Faster");
+		sleepInSecond(3);
+		selectItemInDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Slow");
+		sleepInSecond(3);
+		selectItemInDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Fast");
  
 	}
-	 //3 - Tìm item xem đúng cái đang cần hay không
 
-	
 	//1000ms = 1s
-		public void sleepInSecond(long timeInsecond) {
+	public void sleepInSecond(long timeInsecond) {
 			try {
 				Thread.sleep(timeInsecond * 1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-}
+	
+	// TRánh lặp lại code nhiều lần thì chỉ cần gọi hàm ra để dùng
+	// Đi kèm với tham số
+	// Nếu truyền cứng 1 giá trị vào trong hàm = vô nghĩa
+	// Nên define để dùng đi dùng lại nhiều lần
+	public void selectItemInDropdown(String parentCss, String allItemCss, String expectedTextItem) {
+        //1 - Click vào 1 thẻ bất kì để làm sao cho nó xổ ra hết các item của dropdown
+		driver.findElement(By.cssSelector(parentCss)).click();
+        //2 - Chò cho tất cả các item được load ra thành công
+		// Locator phải lấy để đại diện cho tất cả các item
+		// Lấy đến thẻ chứa text
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(allItemCss)));
+		
+		//Đưa hết tất cả item trong dropdown vào trong 1 list
+		List <WebElement>speedDropdownItems = driver.findElements(By.cssSelector(allItemCss));
+		
+		//3 - Tìm item xem đúng cái đang cần hay không(dùng vòng lặp duyệt qua)
+		for (WebElement tempItem : speedDropdownItems) {
+			String itemText = tempItem.getText();
+			System.out.println(itemText);
+		//4 - Kiểm tra cái text của item đúng với cái mình mong muốn
+			if (itemText.equals(expectedTextItem)) {
+		//5 -  Click vào item đó
+				tempItem.click();
+				// Thoát ra khỏi vòng lặp không xét cho các case 
+				break;
+			}
+		}
+ 
+	}
+ }
