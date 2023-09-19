bb01e488a28c77482e37a4adeb3c43e9af486ae0
This PC <This PC@192.168.1.3>
2023-09-19 19:39:38 +0700

    update

---
+++ src/workdrive/Topic_16_Popup_PartI.java
@@ -0,0 +1,74 @@
+package workdrive;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_16_Popup_PartI {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	//@Test
+	public void TC_01_() {
+		
+	}
+
+	@Test
+	public void TC_02_Fixed_In_DOM() {
+		driver.get("https://ngoaingu24h.vn/");
+		sleepInSecond(10);
+		By loginPopup = By.xpath("//div[@class='modal fade in']//div[@class='modal-content']");
+		//Verify popup is unDisplayed
+		Assert.assertFalse(driver.findElement(loginPopup).isDisplayed());
+		//Click vào button Login
+		driver.findElement(By.cssSelector("button.login_")).click();
+		//Verify popup isDisplayed
+		Assert.assertTrue(driver.findElement(loginPopup).isDisplayed());
+		
+		driver.findElement(By.xpath("//div[@class='modal fade in']//input[@id='account-input']")).sendKeys("automationfc");
+		driver.findElement(By.xpath("//div[@class='modal fade in']//input[@id='password-input']']")).sendKeys("automationfc");
+		driver.findElement(By.xpath("//div[@class='modal fade in']//button[@class='btn-v1 btn-login-v1 buttonLoading']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='modal fade in']//div[@class='row error-login-panel']")).getText(), "Tài khoản không tồn tại!");
+
+	}
+
+	//@Test
+	public void TC_03_Form() {
+		
+	}
+
+	//1000ms = 1s
+			public void sleepInSecond(long timeInsecond) {
+					try {
+						Thread.sleep(timeInsecond * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
