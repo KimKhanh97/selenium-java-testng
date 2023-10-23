93c08e5c46807dd3e587f71317c835ebdae624d4
This PC <This PC@192.168.1.9>
2023-10-23 21:26:08 +0700

    iframe

--- src/workdrive/Topic_17_Frame_Iframe.java
+++ src/workdrive/Topic_17_Frame_Iframe.java
@@ -1,10 +1,13 @@
 package workdrive;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -51,13 +54,37 @@ public class Topic_17_Frame_Iframe {
 	     driver.switchTo().frame("cs_chat_iframe");
 	     //Click chat để show lên chat support
 	     driver.findElement(By.cssSelector("div.button_bar")).click();
+	     sleepInSecond(40);
+	     
+		driver.findElement(By.cssSelector("input.input_name")).sendKeys("Kim Khánh");
+		driver.findElement(By.cssSelector("input.input_phone")).sendKeys("0766625039");
+		new Select(driver.findElement(By.id("serviceSelect"))).selectByVisibleText("TƯ VẤN TUYỂN SINH");
+		driver.findElement(By.name("message")).sendKeys("Tư vấn khóa học Excel");
+		sleepInSecond(3);
 		
-		
+		 //Từ Chat iframe Switch về main page
+		 driver.switchTo().defaultContent();
+		 // Search với từ khóa excel
+		 driver.findElement(By.id("live-search-bar")).sendKeys("Excel");
+		 driver.findElement(By.cssSelector("button.search-button")).click();
+		 
+		 List<WebElement> courseName = driver.findElements(By.cssSelector("div.content>h4"));
+		 
+		 for (WebElement course : courseName) {
+			Assert.assertTrue(course.getText().contains("Excel"));
+		}
+		 
 	}
 
-	@Test
-	public void TC_02_() {
-	}
+	//@Test
+	public void TC_02_HDFC_Bank_Frame() {
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		driver.switchTo().frame("login_page");
+		driver.findElement(By.name("fldLoginUserId")).sendKeys("KimKhanh");
+		driver.findElement(By.cssSelector("a.login-btn")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("")).isDisplayed());
+		driver.findElement(By.cssSelector("")).sendKeys("");
+		}
 
 	
 	//1000ms = 1s
